a8120c1bb0bdd99908a006b5742902a354d09411
nuguiroc <nuno.rocha@agrcanelas.edu.pt>
2025-12-09 16:05:12 +0000

    Elaboração de uma tabela

--- index.php
+++ index.php
@@ -1,7 +1,44 @@
+<table border="1" cellpadding="5" cellspacing="0">
+  <tr>
+    <th>Nome do Aluno</th>
+    <th>Disciplina</th>
+    <th>Nota</th>
+  </tr>
+  <tr><td>Aluno 1</td><td>Disciplina 1</td><td>15</td></tr>
+  <tr><td>Aluno 2</td><td>Disciplina 2</td><td>14</td></tr>
+  <tr><td>Aluno 3</td><td>Disciplina 3</td><td>16</td></tr>
+  <tr><td>Aluno 4</td><td>Disciplina 4</td><td>13</td></tr>
+  <tr><td>Aluno 5</td><td>Disciplina 5</td><td>17</td></tr>
+  <tr><td>Aluno 6</td><td>Disciplina 6</td><td>12</td></tr>
+  <tr><td>Aluno 7</td><td>Disciplina 7</td><td>18</td></tr>
+  <tr><td>Aluno 8</td><td>Disciplina 8</td><td>11</td></tr>
+  <tr><td>Aluno 9</td><td>Disciplina 9</td><td>19</td></tr>
+  <tr><td>Aluno 10</td><td>Disciplina 10</td><td>10</td></tr>
+  <tr><td>Aluno 11</td><td>Disciplina 11</td><td>15</td></tr>
+  <tr><td>Aluno 12</td><td>Disciplina 12</td><td>14</td></tr>
+  <tr><td>Aluno 13</td><td>Disciplina 13</td><td>16</td></tr>
+  <tr><td>Aluno 14</td><td>Disciplina 14</td><td>13</td></tr>
+  <tr><td>Aluno 15</td><td>Disciplina 15</td><td>17</td></tr>
+  <tr><td>Aluno 16</td><td>Disciplina 16</td><td>12</td></tr>
+  <tr><td>Aluno 17</td><td>Disciplina 17</td><td>18</td></tr>
+  <tr><td>Aluno 18</td><td>Disciplina 18</td><td>11</td></tr>
+  <tr><td>Aluno 19</td><td>Disciplina 19</td><td>19</td></tr>
+  <tr><td>Aluno 20</td><td>Disciplina 20</td><td>10</td></tr>
+  <tr><td>Aluno 21</td><td>Disciplina 21</td><td>15</td></tr>
+  <tr><td>Aluno 22</td><td>Disciplina 22</td><td>14</td></tr>
+  <tr><td>Aluno 23</td><td>Disciplina 23</td><td>16</td></tr>
+  <tr><td>Aluno 24</td><td>Disciplina 24</td><td>13</td></tr>
+  <tr><td>Aluno 25</td><td>Disciplina 25</td><td>17</td></tr>
+  <tr><td>Aluno 26</td><td>Disciplina 26</td><td>12</td></tr>
+  <tr><td>Aluno 27</td><td>Disciplina 27</td><td>18</td></tr>
+  <tr><td>Aluno 28</td><td>Disciplina 28</td><td>11</td></tr>
+  <tr><td>Aluno 29</td><td>Disciplina 29</td><td>19</td></tr>
+  <tr><td>Aluno 30</td><td>Disciplina 30</td><td>10</td></tr>
+</table>
 <?php
 echo "Olá Pessoal!!!";
 echo "<BR>funciona bem!";
-echo "Teste de commit"
+echo "Teste de commit";
 echo 5+5;
 echo "Olá!!!";
 echo "Agora fiz no portatil";
